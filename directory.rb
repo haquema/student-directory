f07c0f89acc801c9017078107a95168b0218a7f7
mahaque <haquema@outlook.com>
2022-11-06 15:56:55 +0000

    added a method for inputting student names

--- directory.rb
+++ directory.rb
@@ -1,5 +1,5 @@
 # let's put all students into an array
-students = [
+students_list = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
   {name: "Nurse Ratched", cohort: :november},
@@ -28,6 +28,26 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
