6d26c5747b654a9e48da0cd56b385ffb589ace85
mahaque <haquema@outlook.com>
2022-11-06 23:45:03 +0000

    added method to take arguments from command line

--- directory.rb
+++ directory.rb
@@ -1,3 +1,13 @@
+@students = []
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from student.csv"
+  puts "9. Exit"
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -18,19 +28,17 @@ def input_students
   puts "To finish, just hit return twice"
   # create an empty array
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
   # return the array of students
 end
-
-@students = []
 
 def save_students
   file = File.open("students.csv", "w")
@@ -42,8 +50,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
@@ -51,12 +59,16 @@ def load_students
   file.close
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from student.csv"
-  puts "9. Exit"
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exist?(filename)
+    load_students(filename)
+      puts "Loaded #{@studnets.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
 end
 
 def show_students
@@ -85,7 +97,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
